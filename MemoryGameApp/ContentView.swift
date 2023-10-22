3023023121b3fb73477352c1b15f9433af82fb57
Eligio Cabras <elijjcabras@gmail.com>
2023-10-22 18:00:01 +0200

    Added button for add and remove Cards

--- MemoryGameApp/ContentView.swift
+++ MemoryGameApp/ContentView.swift
@@ -8,31 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    //["🐻","🐶","🐬","🐟","🍀","🚗","🍎","😍","😉","😓","😳","🌟","🎉","🌺","👽","💀","🐵","🐮","🐎","💣"]
+    var emojis : Array<String> = ["🐻","🐶","🐬","🐟","🍀","🚗","😳"]
     
-    var emojis : Array<String> = ["🐻","🐶","🐬","🐟","🍀","🚗","🍎","😍","😉","😓","😳","🌟","🎉","🌺","👽","💀","🐵","🐮","🐎","💣"]
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack(content: {
-            CardView(text: emojis[0])
-            CardView(text: emojis[0])
-            CardView(text: emojis[0])
-        })
-        .foregroundColor(.red)
+        VStack{
+            HStack{
+                ForEach(0..<cardCount, id: \.self){ index in
+                    CardView(text: emojis[index], isFaceUp: true)
+                }
+            }
+            .foregroundColor(.red)
+            HStack{
+                addCard
+                Spacer()
+                removeCard
+            }
+        }
+        .padding()
     }
+    
+    
+    var addCard: some View{
+        //Label("Add Folder", systemImage: "rectangle.stack.badge.plus.fill")
+        Button(action: {
+            if cardCount < emojis.count{
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        }).font(.largeTitle)
+    }
+    
+    var removeCard: some View{
+        //Label("Add Folder", systemImage: "rectangle.stack.badge.plus.fill")
+        Button(action: {
+            if cardCount > 1{
+                cardCount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        }).font(.largeTitle)
+    }
+    
+    
 }
 
 
 struct CardView: View {
     
     var text: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
-        var card = RoundedRectangle(cornerRadius: 12)
-        ZStack(content: {
-            card.fill(.white)
-            card.strokeBorder(lineWidth: 4)
-            Text(text).font(.largeTitle)
-        })
+        let card = RoundedRectangle(cornerRadius: 12)
+        
+        ZStack{
+            if isFaceUp == true{
+                card.fill(.white)
+                card.strokeBorder(lineWidth: 4)
+                Text(text).font(.largeTitle)
+            }else{
+                card.fill(.red)
+            }
+        }.onTapGesture {
+            isFaceUp.toggle() //isFaceUp = !isFaceUp
+        }
         
     }
 }
