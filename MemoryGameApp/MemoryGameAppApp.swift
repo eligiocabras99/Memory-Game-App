e846d4b91a308bdf0c9eb3ae4e1448d403a1ded4
eligiocabras99 <elijjcabras@gmail.com>
2023-10-22 12:24:06 +0200

    Initial Commit with Xcode setup

---
+++ MemoryGameApp/MemoryGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameAppApp.swift
+//  MemoryGameApp
+//
+//  Created by Eligio Cabras on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
